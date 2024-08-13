052950c9007297c53dc26c426a9540aebf90cc49
sori yeon <mi1950@hanmail.net>
2024-08-13 17:57:55 +0900

    portfolio section, sub page button 수정

--- js/common.js
+++ js/common.js
@@ -20,4 +20,18 @@
   }
 
   window.addEventListener("scroll", showItem);
+
+  /* ************************************************************************ */
+
+  const arrowLinks = document.querySelectorAll(".arrowBtns-link");
+
+  arrowLinks.forEach((link) => {
+    link.onmousemove = function (e) {
+      let x = e.pageX - link.offsetLeft;
+      let y = e.pageY - link.offsetTop;
+
+      link.style.setProperty("--x", x + "px");
+      link.style.setProperty("--y", y + "px");
+    };
+  });
 })();
